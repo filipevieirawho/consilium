62b5756eea7207560f946896afc5ed03a2b97a30
Filipe Vieira Who? <filipevieirawho@M4-Air-filipevieirawho.local>
2026-02-26 01:17:24 -0300

    style(users): add people outline icon to user management title

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Usuários') }}
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight flex items-center gap-2">
+            <ion-icon name="people-outline"></ion-icon> {{ __('Usuários') }}
         </h2>
     </x-slot>
 
