f1819647eb27ee5e3dbc0503a17ce1338e4b1134
Filipe Vieira Who? <filipevieirawho@M4-Air-filipevieirawho.local>
2026-02-26 16:05:57 -0300

    style: change old owner text color to black

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -325,7 +325,7 @@
                                                 @if(is_null($item->old_value))
                                                     Proprietário: <strong>{{ \App\Models\User::find($item->new_value)?->name ?? 'Não atribuído' }}</strong>
                                                 @else
-                                                    Proprietário: <strong class="text-gray-500">{{ \App\Models\User::find($item->old_value)?->name ?? 'Não atribuído' }}</strong> 
+                                                    Proprietário: <strong>{{ \App\Models\User::find($item->old_value)?->name ?? 'Não atribuído' }}</strong> 
                                                     <ion-icon name="arrow-forward-outline" class="align-middle text-gray-400 mx-0.5"></ion-icon> 
                                                     <strong>{{ \App\Models\User::find($item->new_value)?->name ?? 'Não atribuído' }}</strong>
                                                 @endif
