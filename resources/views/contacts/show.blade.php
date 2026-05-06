bc8a76176212a8e3d881adb4c0f8e7a51eedd8a8
Filipe Vieira Who? <filipevieirawho@M4-Air-filipevieirawho.local>
2026-05-06 02:10:11 -0300

    Adjust page content padding from py-12 to py-8 across the application

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -20,7 +20,7 @@
         </div>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
             @if(session('success'))
--- resources/views/questionarios/index.blade.php
+++ resources/views/questionarios/index.blade.php
@@ -10,7 +10,7 @@
         </div>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
             @if(session('success'))
